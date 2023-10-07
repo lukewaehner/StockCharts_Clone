69f21afa1ec6466a63e3cd95272cf80b128f8622
Luke Waehner <luke.waehner@gmail.com>
2023-10-06 21:26:28 -0400

    new x-axis slider, fixed ytd
    x-axis slide to target specfici date ranges so moving average
    can be calculated on smaller windows. makes project slower though
    ytd now has a pickle based datetime access system that should hopfully
    update and track the days that have passed, that dont include weekend.
    Im too lazy to incorporate holidays. Will manually update that I guess.
    Well, dont holidays stay the same every year? Can't I just subtract the
    number of holidays on random spots at the end of the year, using a year
    based update variable in pickle (i hate pickle)

--- StockCharts.py
+++ StockCharts.py
@@ -1,45 +1,95 @@
 import yfinance as yf
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import datetime
+import pickle
 
-#Define Symbol and Get Ranges
-ranges = {'day': '1d',
-        'week': '5d',
-        'month': '1mo',
-        'quarter': '3mo',
-        '3 months': '3mo',
-        'half year': '6mo',
-        '6 months': '6mo',
-        '1 year': '1y',
-        'year': '1y',
-        '2 years': '2y',
-        '5 years': '5y',
-        '10 years': '10y',
-        'year to date': 'ytd',
-        'ytd': 'ytd',
+#define symbol and vars
+
+#get vars
+with open('ytdCall.pkl', 'rb') as file:
+        ytdCall = pickle.load(file)
+with open('lastDate.pkl', 'rb') as file:
+        lastDate = pickle.load(file)
+
+today = datetime.date.today()
+year1, week_number1, weekday1 = today.isocalendar()
+year2, week_number2, weekday2 = lastDate.isocalendar()
+
+if weekday1 < 5:
+       if week_number1 > week_number2:
+                inputChange = ytdCall - (today-lastDate).days + ((week_number1 - week_number2)*2)
+       else:
+                inputChange = ytdCall - (today-lastDate).days #calculates the number of days to change
+else:
+        if week_number1 > week_number2:
+         inputChange = ytdCall - (today-lastDate).days + ((week_number1 - week_number2-1)*2) - (weekday1-4)
+        else:   
+         inputChange = ytdCall - (today-lastDate).days - (weekday1-4) #calculates the number of days to change
+if not lastDate == today:
+                lastDate += today - lastDate
+                ytdCall = inputChange
+#dump vars
+with open('lastDate.pkl', 'wb') as file:
+        pickle.dump(lastDate, file)
+with open('ytdCall.pkl', 'wb') as file:
+    pickle.dump(ytdCall, file)
+
+ranges = {'day': 2,
+        'week': 5,
+        'month': 30,
+        'quarter': 90,
+        '3 months': 90,
+        'half year': 120,
+        '6 months': 120,
+        '1 year': 365,
+        'year': 365,
+        '2 years': 730,
+        '5 years': 1826,
+        '10 years': 3652,
+        'year to date': ytdCall,
+        'ytd': ytdCall,
         'max': 'max'
                    }
+dictDate = ranges[input("What start date do you want to  view from: \n")]
 
 #symbol = yf.Ticker(input("Enter a Ticker"))
 symbol = yf.Ticker('TSLA')
-hist = symbol.history(period='1y')
-
+hist = symbol.history(period='max')
+print(hist.index[-192])
 #plotly -> create
 mainChart = make_subplots(specs=[[{"secondary_y": True}]])
 #Candlestick main trace
 mainChart.add_trace(go.Candlestick(x=hist.index,
-                                           open=hist['Open'],
-                                           high=hist['High'],
-                                           low=hist['Low'],
-                                           close=hist['Close']))
+                                open=hist['Open'],
+                                high=hist['High'],
+                                low=hist['Low'],
+                                close=hist['Close']))
 
 #volume trace
-mainChart.add_trace(go.Bar(x=hist.index,y=hist['Volume'],name='Volume'),secondary_y=True)
+hist['diff'] = hist['Close'] - hist['Open']
+hist.loc[hist['diff']>=0, 'color'] = 'green'
+hist.loc[hist['diff']<0, 'color'] = 'red'
+mainChart.add_trace(go.Bar(x=hist.index,y=hist['Volume'],name='Volume', marker={'color': hist['color']}), secondary_y=True)
 #descales volume
 mainChart.update_yaxes(range=[0,5000000000],secondary_y=True)
 mainChart.update_yaxes(visible=False, secondary_y=True)
 
 #MA20 trace
+mainChart.add_trace(go.Scatter(x=hist.index,y=hist['Close'].rolling(window=20).mean(),marker_color='blue',name='20 Day MA'))
 
+#set x-axis initial to show scoped portions
+if not dictDate == "max":
+        #start_date = hist.index[-1 * dictDate]
+        start_date = hist.index[dictDate]
+        end_date = hist.index[-1]
+        mainChart.update_xaxes(range=[start_date, end_date])
+
+#rangeslider tekkkkk
+mainChart.update_layout(xaxis_rangeslider_visible=True)
+
+#update title
+mainChart.update_layout(title={'text': symbol.info["symbol"], 'x':0.5})
+
+#show chart
 mainChart.show()
-#volumeChart.show()
